e200299657be1e4e70bc1cc687a462ccfd5957b7
Yael Chavez <you400@gmail.com>
2020-12-06 02:01:47 -0600

    SQLITE CRUD ADD

--- CarShop/CarShop/Context/DatabaseManager.cs
+++ CarShop/CarShop/Context/DatabaseManager.cs
@@ -4,6 +4,7 @@ using SQLite;
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Linq;
 using System.Text;
 using Xamarin.Forms;
 
@@ -17,29 +18,29 @@ namespace CarShop.Context
         {
             db = DependencyService.Get<ISQLite>().GetConnection();
 
-            if(!(TableExists("Car")))
+            if(!(TableExists("FavoriteCar")))
                  db.CreateTable<Car>();
         }
 
         public bool TableExists(string tableName)
         {
-            SQLite.TableMapping map = new TableMapping(typeof(SqlDbType)); 
+            TableMapping map = new TableMapping(typeof(SqlDbType)); 
             object[] ps = new object[0]; 
 
             int tableCount = db.Query(map, "SELECT * FROM sqlite_master WHERE type = 'table' AND name = '" + tableName + "'", ps).Count; 
             if (tableCount == 0)
-            {
                 return false;
-            }
             else if (tableCount == 1)
-            {
                 return true;
-            }
             else
-            {
                 throw new Exception($"Hay mas de una tabla con el nombre {tableName} en la base de datos", null);
-            }
-
         }
+
+        public List<Car> GetFavoriteCars() => db.Query<Car>("Select * from FavoriteCar");
+
+        public Car GetFavoriteCars(int id) => db.Query<Car>($"Select * from FavoriteCar where id = {id}").First();
+
+        public void AddFavoriteCar(int id) => db.Insert(GetFavoriteCars(id));
+
     }
 }
